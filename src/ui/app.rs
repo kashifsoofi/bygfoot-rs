b2bec2d74383487784af75f632efe53ebbf84ef4
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-02-11 01:55:53 +0000

    Methods to show window

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,6 +1,6 @@
 use gtk::{glib, prelude::*, subclass::prelude::*};
 
-use crate::ui::window::SplashWindow;
+use crate::ui::window::{SplashWindow, StartupWindow};
 
 #[derive(Debug, Default)]
 // By implementing Default we don't have to provide a `new` fn in our
@@ -19,8 +19,19 @@ impl ApplicationImpl for App {
     fn activate(&self) {
         self.parent_activate();
         // We create our window at activation stage
-        let splash_window = SplashWindow::new(&*self.obj());
-        splash_window.present();
+        self.show_splash();
     }
 }
 impl GtkApplicationImpl for App {}
+
+impl App {
+    pub fn show_splash(&self) {
+        let splash_window = SplashWindow::new(&*self.obj());
+        splash_window.present();
+    }
+
+    pub fn show_startup(&self) {
+        let startup_window = StartupWindow::new(&*self.obj());
+        startup_window.present();
+    }
+}
--- src/ui/window/splash.rs
+++ src/ui/window/splash.rs
@@ -88,6 +88,7 @@ impl SplashWindow {
     
     #[template_callback]
     fn on_new_game_clicked(&self, _: &Button) {
+        //self.application.show_startup();
         let startup_window = super::StartupWindow::new(&self.application);
         startup_window.present();
     }
